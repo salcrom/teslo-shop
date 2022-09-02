fd9645a167079bb8a3481638878173c28714c759
Sergio Alcantara Romero <seya.28@gmail.com>
2022-09-02 19:25:55 +0200

    Agragado custom Hooks y configuración Global SWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
+
+import '../styles/globals.css'
 import { ThemeProvider } from '@mui/material'
 import { lightTheme } from 'themes'
 
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={ lightTheme }>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        // refreshInterval: 3000,
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <ThemeProvider theme={ lightTheme }>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,32 @@ import type { NextPage } from 'next'
 import { Typography } from '@mui/material'
 
 import { ShopLayout } from 'components/layouts'
-import { initialData } from 'database/products'
+
 import { ProductList } from 'components/products'
+import { useProducts } from 'hooks'
 
 
 
-const Home: NextPage = () => {
+
+
+
+const HomePage: NextPage = () => {
+
+  const { products, isLoading } = useProducts('/products')
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <ProductList
-        products={ initialData.products as any }
-      />
+      {
+        isLoading
+          ? <h1>Cargando...</h1>
+          : <ProductList products={ products } />
+      }
 
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
