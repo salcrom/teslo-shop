693431db1fb014dc0decb0597b56ec8a875365df
Sergio Alcantara Romero <seya.28@gmail.com>
2022-09-04 20:09:24 +0200

    Añadida interface del modelo user

---
+++ interfaces/user.ts
@@ -0,0 +1,12 @@
+
+
+export interface IUser {
+    _id      : string;
+    name     : string;
+    email    : string;
+    password?: string;
+    role     : string;
+
+    createAt? : string;
+    updatedAt?: string;
+}
