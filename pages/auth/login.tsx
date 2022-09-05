e06aec91df9d22dac77606d941dfbe4d69cbac6d
Sergio Alcantara Romero <seya.28@gmail.com>
2022-09-05 19:09:09 +0200

    Añadido RHF en login y validación en el frontend

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,41 +1,92 @@
 import NextLink from 'next/link';
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { useForm } from 'react-hook-form';
 
 import { AuthLayout } from 'components/layouts'
+import { validations } from 'utils';
+
+
+type FormData = {
+    email: string,
+    password: string,
+};
+
 
 
 
 const LoginPage = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+    console.log(errors);
+
+    const onLoginUser = ( data: FormData ) => {
+        console.log({data});
+    }
+
+
     return (
         <AuthLayout title={'Ingresar'}>
-            <Box sx={{ width: 350, padding: '10px 20px' }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <Typography variant='h1' component='h1'>Iniciar Sesión</Typography>
-                    </Grid>
+            <form onSubmit={ handleSubmit(onLoginUser) }>
+                <Box sx={{ width: 350, padding: '10px 20px' }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant='h1' component='h1'>Iniciar Sesión</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                type='email'
+                                label='Correo electrónico'
+                                variant='filled'
+                                fullWidth
+                                {
+                                    ...register('email', {
+                                        required: 'Este campo es requerido',
+                                        validate: (val) => validations.isEmail(val)
+                                    })
+                                }
+                                error={ !!errors.email }
+                                helperText={ errors.email?.message }
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label='Contraseña'
+                                type='password'
+                                variant='filled'
+                                fullWidth
+                                {
+                                    ...register('password', {
+                                        required: 'Este campo es requerido',
+                                        minLength: { value: 6, message: 'Mínimo 6 caracteres' }
+                                    })
+                                }
+                                error={ !!errors.password }
+                                helperText={ errors.password?.message }
+                            />
+                        </Grid>
 
-                    <Grid item xs={12}>
-                        <TextField label='Correo electrónico' variant='filled' fullWidth />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField label='Contraseña' type='password' variant='filled' fullWidth />
-                    </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                type='submit'
+                                color='secondary'
+                                className='circular-btn'
+                                size='large'
+                                fullWidth
+                            >
+                                Ingresar
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} display='flex' justifyContent='end'>
+                            <NextLink href='/auth/register' passHref>
+                                <Link underline='always'>
+                                    ¿No tienes cuenta?
+                                </Link>
+                            </NextLink>
+                        </Grid>
 
-                    <Grid item xs={12}>
-                        <Button color='secondary' className='circular-btn' size='large' fullWidth>
-                            Ingresar
-                        </Button>
                     </Grid>
-                    <Grid item xs={12} display='flex' justifyContent='end'>
-                        <NextLink href='/auth/register' passHref>
-                            <Link underline='always'>
-                                ¿No tienes cuenta?
-                            </Link>
-                        </NextLink>
-                    </Grid>
-
-                </Grid>
-            </Box>
+                </Box>
+            </form>
         </AuthLayout>
     )
 }
