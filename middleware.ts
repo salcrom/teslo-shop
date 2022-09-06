22d2048555a2e2ec2b37bfff62fe8cf13e782088
Sergio Alcantara Romero <seya.28@gmail.com>
2022-09-06 14:48:01 +0200

    Middleware de autenticación

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import * as jose from 'jose'
+
+
+export async function middleware( req: NextRequest ) {
+
+    try {
+        await jose.jwtVerify( req.cookies.get('token') as string, new TextEncoder().encode( process.env.JWT_SECRET_SEED ));
+
+        return NextResponse.next();
+
+    } catch (error) {
+
+        return NextResponse.redirect(`http://localhost:3000/auth/login?p=${ config.matcher }`);
+
+    }
+}
+
+export const config = {
+    matcher: '/checkout/address'
+}
--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -63,34 +63,34 @@ const addressPage = () => {
 
 // You should use getServerSideProps when:
 // - Only if you need to pre-render a page whose data must be fetched at request time
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+// export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 
-    const { token = '' } = req.cookies;
+//     const { token = '' } = req.cookies;
 
-    let isValidToken = false;
+//     let isValidToken = false;
 
-    try {
-        await jwt.isValidToken( token );
-        isValidToken = true;
-    } catch (error) {
-        isValidToken = false;
-    }
+//     try {
+//         await jwt.isValidToken( token );
+//         isValidToken = true;
+//     } catch (error) {
+//         isValidToken = false;
+//     }
 
-    if ( !isValidToken ) {
-        return {
-            redirect: {
-                destination: '/auth/login?p=/checkout/address',
-                permanent: false,
-            }
-        }
-    }
+//     if ( !isValidToken ) {
+//         return {
+//             redirect: {
+//                 destination: '/auth/login?p=/checkout/address',
+//                 permanent: false,
+//             }
+//         }
+//     }
 
-    return {
-        props: {
+//     return {
+//         props: {
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
 
 
