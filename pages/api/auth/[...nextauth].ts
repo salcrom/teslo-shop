61eaecd45f608457fc1fd0c78797f3fb089196c4
Sergio Alcantara Romero <seya.28@gmail.com>
2022-09-07 20:55:02 +0200

    Añadido proveedo credenciales

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,34 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
 
 export const authOptions = {
   // Configure one or more authentication providers
     providers: [
-        GithubProvider({
+      Credentials({
+        name: 'Custom Login',
+        credentials: {
+          email: { label: 'Correo:', type: 'email', placeholder: 'correo@google.com' },
+          password: { label: 'Contraseña:', type: 'password', placeholder: 'Contraseña' },
+        },
+        async authorize(credentials) {
+          console.log({credentials});
+          // TODO: validar contra base de datos
+
+          return { name: 'Juan', correo: 'juan@cgoogle.com', role: 'admin' };
+        }
+      }),
+      // ...add more providers here
+      GithubProvider({
         clientId: process.env.GITHUB_ID!,
         clientSecret: process.env.GITHUB_SECRET!,
         }),
-        // ...add more providers here
     ],
+
+    // Callbacks
+    callback: {
+      
+    }
 }
 
 export default NextAuth(authOptions)
