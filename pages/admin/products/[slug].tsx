84b4f83aff3f515cfa262ebb87e36c8254c6e597
Sergio Alcantara Romero <seya.28@gmail.com>
2022-09-15 17:01:44 +0200

    Conectar los campos con React Hook Form

--- pages/admin/products/[slug].tsx
+++ pages/admin/products/[slug].tsx
@@ -1,15 +1,32 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import { GetServerSideProps } from 'next'
+import { useForm } from 'react-hook-form';
+
+import { DriveFileRenameOutline, SaveOutlined, UploadOutlined } from '@mui/icons-material';
+import { Box, Button, capitalize, Card, CardActions, CardMedia, Checkbox, Chip, Divider, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, ListItem, Paper, Radio, RadioGroup, TextField } from '@mui/material';
+
 import { AdminLayout } from '../../../components/layouts'
 import { IProduct } from '../../../interfaces';
-import { DriveFileRenameOutline, SaveOutlined, UploadOutlined } from '@mui/icons-material';
 import { dbProducts } from '../../../database';
-import { Box, Button, capitalize, Card, CardActions, CardMedia, Checkbox, Chip, Divider, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, ListItem, Paper, Radio, RadioGroup, TextField } from '@mui/material';
 
 
 const validTypes  = ['shirts','pants','hoodies','hats']
 const validGender = ['men','women','kid','unisex']
 const validSizes = ['XS','S','M','L','XL','XXL','XXXL']
+
+interface FormData {
+    _id?       : string;
+    description: string;
+    images     : string[];
+    inStock    : number;
+    price      : number;
+    sizes      : string[];
+    slug       : string;
+    tags       : string[];
+    title      : string;
+    type       : string;
+    gender     : string;
+}
 
 interface Props {
     product: IProduct;
@@ -17,8 +34,16 @@ interface Props {
 
 const ProductAdminPage:FC<Props> = ({ product }) => {
 
+    const { register, handleSubmit, formState:{ errors } } = useForm<FormData>({
+        defaultValues: product
+    })
+
     const onDeleteTag = ( tag: string ) => {
 
+    }
+
+    const onSubmit = ( form: FormData ) => {
+        console.log( {form} );
     }
 
     return (
@@ -27,7 +52,7 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
             subTitle={`Editando: ${ product.title }`}
             icon={ <DriveFileRenameOutline /> }
         >
-            <form>
+            <form onSubmit={ handleSubmit( onSubmit ) }>
                 <Box display='flex' justifyContent='end' sx={{ mb: 1 }}>
                     <Button
                         color="secondary"
@@ -48,12 +73,12 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                             variant="filled"
                             fullWidth
                             sx={{ mb: 1 }}
-                            // { ...register('name', {
-                            //     required: 'Este campo es requerido',
-                            //     minLength: { value: 2, message: 'Mínimo 2 caracteres' }
-                            // })}
-                            // error={ !!errors.name }
-                            // helperText={ errors.name?.message }
+                            { ...register('title', {
+                                required: 'Este campo es requerido',
+                                minLength: { value: 2, message: 'Mínimo 2 caracteres' }
+                            })}
+                            error={ !!errors.title }
+                            helperText={ errors.title?.message }
                         />
 
                         <TextField
@@ -62,6 +87,11 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                             fullWidth
                             multiline
                             sx={{ mb: 1 }}
+                            { ...register('description', {
+                                required: 'Este campo es requerido',
+                            })}
+                            error={ !!errors.description }
+                            helperText={ errors.description?.message }
                         />
 
                         <TextField
@@ -70,6 +100,12 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                             variant="filled"
                             fullWidth
                             sx={{ mb: 1 }}
+                            { ...register('inStock', {
+                                required: 'Este campo es requerido',
+                                min: { value: 0, message: 'Mínimo de valor cero' }
+                            })}
+                            error={ !!errors.inStock }
+                            helperText={ errors.inStock?.message }
                         />
 
                         <TextField
@@ -78,6 +114,12 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                             variant="filled"
                             fullWidth
                             sx={{ mb: 1 }}
+                            { ...register('price', {
+                                required: 'Este campo es requerido',
+                                min: { value: 0, message: 'Mínimo de valor cero' }
+                            })}
+                            error={ !!errors.price }
+                            helperText={ errors.price?.message }
                         />
 
                         <Divider sx={{ my: 1 }} />
@@ -140,6 +182,12 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
                             variant="filled"
                             fullWidth
                             sx={{ mb: 1 }}
+                            { ...register('slug', {
+                                required: 'Este campo es requerido',
+                                validate: (val) => val.trim().includes(' ') ? 'No puede tener espacios en blanco' : undefined
+                            })}
+                            error={ !!errors.slug }
+                            helperText={ errors.slug?.message }
                         />
 
                         <TextField
