179ad0e4bec79900e59ac84257e844eb2cfc282d
Sergio Alcantara Romero <seya.28@gmail.com>
2022-09-16 07:18:19 +0200

    REST - Crear producto

--- models/Product.ts
+++ models/Product.ts
@@ -2,7 +2,7 @@ import { IProduct } from 'interfaces';
 import mongoose, { Schema, model, Model } from 'mongoose';
 
 const productSchema = new Schema({
-    description: { type: String, required: true },
+    description: { type: String, required: true, default: '' },
     images: [{ type: String }],
     inStock: { type: Number, required: true, default: 0 },
     price: { type: Number, required: true, default: 0 },
@@ -15,19 +15,22 @@ const productSchema = new Schema({
     }],
     slug: { type: String, required: true, unique: true },
     tags: [{ type:String }],
-    title: { type: String, required: true },
+    title: { type: String, required: true, default: '' },
     type: {
         type: String,
         enum: {
             values: ['shirts','pants','hoodies','hats'],
             message: '{VALUE} no es un tipo válido'
-        }},
+        },
+        default: 'shrts'
+    },
     gender: {
         type: String,
         enum: {
             values: ['men','women','kid','unisex'],
             message: '{VALUE} no es un género válido'
-        }
+        },
+        default: 'women'
     },
 },{
     timestamps: true
--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -23,6 +23,7 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
             return updatedProduct(req, res)
 
         case 'POST':
+            return createProduct(req, res)
 
         default:
             return res.status(200).json({ message: 'Example' })
@@ -80,4 +81,36 @@ const updatedProduct = async(req: NextApiRequest, res: NextApiResponse<Data>) =>
         return res.status(400).json({ message: 'Revisar la consola del servidor' })
     }
 
+}
+
+
+const createProduct = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    
+    const { images = [] } = req.body as IProduct;
+
+    if ( images.length < 2 ) {
+        return res.status(400).json({ message: 'El producto necesita al menos 2 imágenes' });
+    }
+
+    // TODO: posiblimente tendremos un localhost:3000//products/asdaasd.jpg
+
+    try {
+        await db.connect();
+        const productInDB = await Product.findOne({ slug: req.body.slug })
+        if ( productInDB ) {
+            await db.disconnect();
+            return res.status(400).json({ message: 'Ya existe un producto con ese slug' });
+        }
+
+        const product = new Product( req.body );
+        await product.save();
+        await db.disconnect();
+
+        res.status(201).json( product );
+
+    } catch (error) {
+        console.log(error);
+        await db.disconnect();
+        return res.status(400).json({ message: 'Revisar logs del servidor' })
+    }
 }
